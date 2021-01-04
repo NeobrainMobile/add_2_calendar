ca70f046442e232970ad0d6f6eac75db17fd4459
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2021-01-04 14:47:14 +0000

    Update Add2CalendarPlugin.java

--- android/src/main/java/com/javih/add2calendar/Add2CalendarPlugin.java
+++ android/src/main/java/com/javih/add2calendar/Add2CalendarPlugin.java
@@ -18,15 +18,24 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+/** Add2CalendarPlugin */
+public class Add2CalendarPlugin implements MethodCallHandler {
+    private final Registrar mRegistrar;
 /** Add2CalendarPlugin **/
 public class Add2CalendarPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
 
+    public Add2CalendarPlugin(Registrar registrar) {
+        mRegistrar = registrar;
+    }
     private MethodChannel channel;
     private Activity activity;
     private Context context;
 
+    /** Plugin registration. */
     /** backward compatibility with embedding v1 **/
     public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter.javih.com/add_2_calendar");
+        channel.setMethodCallHandler(new Add2CalendarPlugin(registrar));
         Add2CalendarPlugin plugin = new Add2CalendarPlugin();
         plugin.activity = registrar.activity();
         plugin.context = registrar.context();
@@ -71,31 +80,31 @@ public class Add2CalendarPlugin implements MethodCallHandler, FlutterPlugin, Act
     }
 
     @Override
+    public void onMethodCall(MethodCall call, Result result) {
     public void onMethodCall(MethodCall call, @NonNull Result result) {
         if (call.method.equals("add2Cal")) {
             try {
                 insert((String) call.argument("title"), (String) call.argument("desc"), (String) call.argument("location"), (long) call.argument("startDate"), (long) call.argument("endDate"), (String) call.argument("timeZone"), (boolean) call.argument("allDay"));
-                result.success(true);
-            } catch (NullPointerException e) {
-                result.error("Exception ocurred in Android code", e.getMessage(), false);
-            }
-        } else {
-            result.notImplemented();
-        }
+@@ -40,8 +85,8 @@ public void onMethodCall(MethodCall call, Result result) {
     }
 
     @SuppressLint("NewApi")
+    public void insert(String title, String desc, String loc, long start, long end, String timeZone, boolean allDay) {
+        Context context = getActiveContext();
     private void insert(String title, String desc, String loc, long start, long end, String timeZone, boolean allDay) {
         Context mContext = activity != null ? activity : context;
         Intent intent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
         intent.putExtra(CalendarContract.Events.TITLE, title);
         intent.putExtra(CalendarContract.Events.DESCRIPTION, desc);
-        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, loc);
-        intent.putExtra(CalendarContract.Events.EVENT_TIMEZONE, timeZone);
-        intent.putExtra(CalendarContract.Events.EVENT_END_TIMEZONE, timeZone);
+@@ -51,10 +96,7 @@ public void insert(String title, String desc, String loc, long start, long end,
         intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, start);
         intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end);
         intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, allDay);
+        context.startActivity(intent);
+    }
+
+    private Context getActiveContext() {
+        return (mRegistrar.activity() != null) ? mRegistrar.activity() : mRegistrar.context();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(intent);
     }
